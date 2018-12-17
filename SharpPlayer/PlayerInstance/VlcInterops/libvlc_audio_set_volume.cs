d8642187d67fab0f23e8212314deba777d1bcc74
DK189 <tran.trong@outlook.com>
2018-12-17 21:25:35 +0700

    Revert "Init unrelease version."
    This reverts commit 575e928aa6964e4d2ec8e6c3395dacad4e2f42bd.

--- SharpPlayer/PlayerInstance/VlcInterops/libvlc_audio_set_volume.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-namespace SharpPlayer.PlayerInstance.VlcInterops
-{
-    internal delegate void libvlc_audio_set_volume(IntPtr mediaPlayerInstance, int volume);
-}
--- SharpPlayer/PlayerInstance/VlcInterops/libvlc_media_player_get_time.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-namespace SharpPlayer.PlayerInstance.VlcInterops
-{
-    internal delegate long libvlc_media_player_get_time(IntPtr mediaPlayerInstance);
-}
